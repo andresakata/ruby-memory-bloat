88f0f155ee4b1645d0b031159145d73486ffbd58
Andre Sakata <andregsakata@gmail.com>
2018-07-28 19:49:15 -0300

    Remove aux script

--- aux.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'fragmentation.rb'
-FragmentationCreator.new(file_name: 'glibc.log').execute
-
-require_relative 'fragmentation.rb'
-FragmentationCreator.new(file_name: 'jemalloc.log').execute
