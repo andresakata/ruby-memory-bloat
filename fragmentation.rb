d07083e101bc92de215456ee6de08b46f63c0101
André Guimarães Sakata <andregsakata@gmail.com>
2019-01-26 19:41:52 -0200

    Change default parameters and log execution time

--- fragmentation.rb
+++ fragmentation.rb
@@ -3,7 +3,7 @@ require 'get_process_mem'
 require 'logger'
 
 class FragmentationCreator
-  def initialize(file_name: 'memory.log', threads: 25, jobs: 5000, array_size: 1024)
+  def initialize(file_name: 'memory.log', threads: 30, jobs: 10000, array_size: 1024)
     @threads = threads
     @jobs = jobs
     @array_size = array_size
@@ -14,18 +14,20 @@ class FragmentationCreator
 
   def execute
     GC.start
+    start_time = Time.now
     pool = Concurrent::FixedThreadPool.new(@threads)
     (1..@jobs).each do |index|
       pool.post { thread_process(index % 25 == 0) }
     end
     pool.shutdown
     pool.wait_for_termination
+    @logger.info(Time.now - start_time)
   end
 
   def thread_process(big_array)
-    array_size = big_array ? @array_size * 100 : @array_size
+    array_size = big_array ? @array_size * 150 : @array_size
     array = Array.new(array_size) { 'a' * 1024 }
-    sleep(rand(0.1..0.5))
+    # sleep(rand(0.1..0.5))
     @logger.info(@mem.mb.to_s)
     print(big_array ? 'x' : '.')
   end
